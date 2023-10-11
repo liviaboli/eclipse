fd3815f37b5c89cffb66eba8a6e08702d2feca8a
Liviaboli <79103453+liviaboli@users.noreply.github.com>
2023-10-11 15:51:01 -0300

    goku seu ku

--- Hello GitHub/src/liviaboli/git/HelloGit.java
+++ Hello GitHub/src/liviaboli/git/HelloGit.java
@@ -13,7 +13,7 @@ public class HelloGit {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("OIIII eu sou o gokuuuu");
+		System.out.println("OIIII eu sou o goku seu ku");
 
 	}
 
