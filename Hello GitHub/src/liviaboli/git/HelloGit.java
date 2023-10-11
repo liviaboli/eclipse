94f2b3517521fa98af131b5ed2f2e260245090c3
Liviaboli <79103453+liviaboli@users.noreply.github.com>
2023-10-11 16:14:39 -0300

    gokuzin chann

--- Hello GitHub/src/liviaboli/git/HelloGit.java
+++ Hello GitHub/src/liviaboli/git/HelloGit.java
@@ -13,7 +13,7 @@ public class HelloGit {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("OIIII eu sou o goku seu ku");
+		System.out.println("OIIII eu sou o goku seu kuzin");
 
 	}
 
